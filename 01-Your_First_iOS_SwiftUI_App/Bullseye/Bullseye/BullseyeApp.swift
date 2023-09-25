b22179e61ab18d2c1185d7a24354dfd435e307dd
Anne Freitas <annekarinyfreitas@gmail.com>
2023-09-25 10:40:31 -0300

    Create Your First iOS SwiftUI App folder. Create Bullseye project.

---
+++ 01-Your_First_iOS_SwiftUI_App/Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Anne Freitas on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
