d40990fb6f93b4a807c755182f99d1c96223e8d1
Anne Freitas <annekarinyfreitas@gmail.com>
2023-09-27 04:51:36 -0300

    Create tests target. Fix typo.

--- 01-Your_First_iOS_SwiftUI_App/Bullseye/Bullseye/Models/Game.swift
+++ 01-Your_First_iOS_SwiftUI_App/Bullseye/Bullseye/Models/Game.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct Game {
-  var target: Int = 37
+  var target: Int = Int.random(in: 1...100)
   var score: Int = 0
-  var rount: Int = 1
+  var round: Int = 1
   
   func points(sliderValue: Int) -> Int {
     return 999
