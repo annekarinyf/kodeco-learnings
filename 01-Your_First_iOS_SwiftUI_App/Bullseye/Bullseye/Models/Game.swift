9a8da9e080fcd74fbd9e6eedef0f9a14a1757055
Anne Freitas <annekarinyfreitas@gmail.com>
2023-09-26 21:18:15 -0300

    Create model for game.

---
+++ 01-Your_First_iOS_SwiftUI_App/Bullseye/Bullseye/Models/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Anne Freitas on 26/09/23.
+//
+
+import Foundation
+
+struct Game {
+  var target: Int = 37
+  var score: Int = 0
+  var rount: Int = 1
+}
