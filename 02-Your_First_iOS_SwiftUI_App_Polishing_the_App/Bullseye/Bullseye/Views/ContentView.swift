7b04fb10b9697282797041e425f18c9ccd24f6b9
Anne Freitas <annekarinyfreitas@gmail.com>
2023-10-23 16:45:42 -0300

    Add background and foreground color modifiers. Add padding.

--- 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Views/ContentView.swift
+++ 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Views/ContentView.swift
@@ -8,50 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var alertIsVisible = false
-  @State private var sliderValue = 50.0
-  @State private var game = Game()
-  
-  var body: some View {
-    VStack {
-      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-        .bold()
-        .multilineTextAlignment(.center)
-        .lineSpacing(4.0)
-        .font(.footnote)
-        .kerning(2.0)
-      Text(String(game.target))
-        .font(.largeTitle)
-        .fontWeight(.black)
-        .kerning(-1.0)
-      HStack {
-        Text("1")
-          .bold()
-          .font(.body)
-        Slider(value: $sliderValue, in: 1.0...100.0)
-        Text("100")
-          .bold()
-          .font(.body)
-      }
-      Button(action: {
-        print("Hello, SwiftUI!")
-        alertIsVisible = true
-      }) {
-        Text("Hit me")
-      }
-      .alert(isPresented: $alertIsVisible) {
-        return Alert(
-          title: Text("Hello there"),
-          message: Text("The slider's value is \(Int(sliderValue.rounded())). You scored \(game.points(sliderValue: Int(sliderValue))) this round."),
-          dismissButton: .default(Text("Awesome"))
-        )
-      }
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
+    
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+                .kerning(2.0)
+                .padding(.horizontal, 30)
+            Text(String(game.target))
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .kerning(-1.0)
+            HStack {
+                Text("1")
+                    .bold()
+                    .font(.body)
+                Slider(value: $sliderValue, in: 1.0...100.0)
+                Text("100")
+                    .bold()
+                    .font(.body)
+            }
+            .padding(.all)
+            Button("Hit me".uppercased()) {
+                alertIsVisible = true
+            }
+            .padding(20)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(21)
+            .bold()
+            .font(.title3)
+            .alert(isPresented: $alertIsVisible) {
+                return Alert(
+                    title: Text("Hello there"),
+                    message: Text("The slider's value is \(Int(sliderValue.rounded())). You scored \(game.points(sliderValue: Int(sliderValue))) this round."),
+                    dismissButton: .default(Text("Awesome"))
+                )
+            }
+        }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
