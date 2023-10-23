7f2e542aca5299354410b90d27308a5452ce6a0e
Anne Freitas <annekarinyfreitas@gmail.com>
2023-10-23 16:23:48 -0300

    Create polishing the app course folder. Copy project from first course

---
+++ 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Models/Game.swift
@@ -0,0 +1,24 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Anne Freitas on 26/09/23.
+//
+
+import Foundation
+
+struct Game {
+  private struct Contants {
+    static let minTargetValue = 1
+    static let maxTargetValue = 100
+  }
+  
+  var target = Int.random(in: Contants.minTargetValue...Contants.maxTargetValue)
+  var score = 0
+  var round = 1
+  
+  func points(sliderValue: Int) -> Int {
+    let difference = abs(target - sliderValue)
+    return Contants.maxTargetValue - abs(target - sliderValue)
+  }
+}
---
+++ 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Views/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Anne Freitas on 25/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var alertIsVisible = false
+  @State private var sliderValue = 50.0
+  @State private var game = Game()
+  
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+      Text(String(game.target))
+        .font(.largeTitle)
+        .fontWeight(.black)
+        .kerning(-1.0)
+      HStack {
+        Text("1")
+          .bold()
+          .font(.body)
+        Slider(value: $sliderValue, in: 1.0...100.0)
+        Text("100")
+          .bold()
+          .font(.body)
+      }
+      Button(action: {
+        print("Hello, SwiftUI!")
+        alertIsVisible = true
+      }) {
+        Text("Hit me")
+      }
+      .alert(isPresented: $alertIsVisible) {
+        return Alert(
+          title: Text("Hello there"),
+          message: Text("The slider's value is \(Int(sliderValue.rounded())). You scored \(game.points(sliderValue: Int(sliderValue))) this round."),
+          dismissButton: .default(Text("Awesome"))
+        )
+      }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
