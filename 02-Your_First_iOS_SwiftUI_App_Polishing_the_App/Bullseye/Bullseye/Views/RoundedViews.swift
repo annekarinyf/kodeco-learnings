11f617cdbb18075f5894fc8f21452e48a3cff0ff
Anne Freitas <annekarinyfreitas@gmail.com>
2023-12-01 16:48:09 -0300

    Create and customize rounded views

---
+++ 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Views/RoundedViews.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedViews.swift
+//  Bullseye
+//
+//  Created by Anne Freitas on 01/12/23.
+//
+
+import SwiftUI
+
+struct RoundedViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundedViews()
+}
