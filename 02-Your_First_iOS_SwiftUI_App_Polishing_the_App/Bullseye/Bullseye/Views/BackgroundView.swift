d0fcc784380f9398f2db8b78408736125e506211
Anne Freitas <annekarinyfreitas@gmail.com>
2023-12-01 16:59:57 -0300

    Create BackgroundView

---
+++ 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Views/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  Bullseye
+//
+//  Created by Anne Freitas on 01/12/23.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
