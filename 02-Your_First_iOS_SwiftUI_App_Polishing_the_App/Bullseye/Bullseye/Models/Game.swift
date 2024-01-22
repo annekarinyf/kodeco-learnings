84be42ce249e6b4442e857c8307d69311738b8cc
Anne Freitas <annekarinyfreitas@gmail.com>
2024-01-22 17:47:28 -0300

    NIT: Fix typo

--- 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Models/Game.swift
+++ 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Models/Game.swift
@@ -8,17 +8,22 @@
 import Foundation
 
 struct Game {
-  private struct Contants {
-    static let minTargetValue = 1
-    static let maxTargetValue = 100
-  }
-  
-  var target = Int.random(in: Contants.minTargetValue...Contants.maxTargetValue)
-  var score = 0
-  var round = 1
-  
-  func points(sliderValue: Int) -> Int {
-    let difference = abs(target - sliderValue)
-    return Contants.maxTargetValue - abs(target - sliderValue)
-  }
+    private struct Constants {
+        static let minTargetValue = 1
+        static let maxTargetValue = 100
+    }
+    
+    var target = Int.random(in: Constants.minTargetValue...Constants.maxTargetValue)
+    var score = 0
+    var round = 1
+    
+    func points(sliderValue: Int) -> Int {
+        let difference = abs(target - sliderValue)
+        return Constants.maxTargetValue - abs(target - sliderValue)
+    }
+    
+    mutating func startNewRound(points: Int) {
+        self.score += points
+        self.round += 1
+    }
 }
