eba1a9c68af5767bae6fe900c052ebdc4971dead
Anne Freitas <annekarinyfreitas@gmail.com>
2024-02-01 12:01:04 -0300

    Add transition animations to button

--- 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Views/PointsView.swift
+++ 02-Your_First_iOS_SwiftUI_App_Polishing_the_App/Bullseye/Bullseye/Views/PointsView.swift
@@ -21,7 +21,9 @@ struct PointsView: View {
             BigNumberText(text: String(roundedValue))
             BodyText(text: "You score \(points) Points\n 😻😻😻")
             Button {
-                alertIsVisible = false
+                withAnimation {
+                    alertIsVisible = false
+                }
                 game.startNewRound(points: points)
             } label: {
                 ButtonText(text: "Start New Round")
